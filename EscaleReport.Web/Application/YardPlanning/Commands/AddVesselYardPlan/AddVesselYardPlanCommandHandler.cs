fcd9f241c6fde33308596f8f54d7c78f1320a683
Konate Mamadou <120653410+MamadouKernel@users.noreply.github.com>
2026-07-26 20:15:18 +0000

    Reduce repetitive operational data entry

--- EscaleReport.Web/Application/YardPlanning/Commands/AddVesselYardPlan/AddVesselYardPlanCommandHandler.cs
+++ EscaleReport.Web/Application/YardPlanning/Commands/AddVesselYardPlan/AddVesselYardPlanCommandHandler.cs
@@ -3,6 +3,7 @@ using EscaleReport.Web.Application.Common.Interfaces;
 using EscaleReport.Web.Domain.Identity;
 using EscaleReport.Web.Domain.YardPlanning;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace EscaleReport.Web.Application.YardPlanning.Commands.AddVesselYardPlan;
 
@@ -17,10 +18,15 @@ public class AddVesselYardPlanCommandHandler(
             throw new ForbiddenAccessException(Permissions.SaisirDonneesModule);
         }
 
+        var escale = await dbContext.Escales.AsNoTracking()
+            .FirstOrDefaultAsync(e => e.Id == request.EscaleId, cancellationToken)
+            ?? throw new KeyNotFoundException("Escale introuvable.");
+
         var plan = new VesselYardPlan
         {
             EscaleId = request.EscaleId,
-            ServiceMaritime = request.ServiceMaritime,
+            // Le service est déjà porté par l'escale : aucune ressaisie utilisateur.
+            ServiceMaritime = escale.LigneMaritime,
             ZoneDebarquement = request.ZoneDebarquement,
             ReefersImport = request.ReefersImport,
             ReefersExport = request.ReefersExport,
